74ddc17c62deb88d9278c3cfd73832e1289abfd8
bergersride <bergersride@bergersride.com>
2020-12-09 17:39:54 -0700

    1 file modified

--- index.tsx
+++ index.tsx
@@ -6,11 +6,7 @@ import {
   Icon,
   Intent,
   ITreeNode,
-  Button,
-  ButtonGroup,
-  Popover,
   Position,
-  Menu,
   Classes
 } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
@@ -95,97 +91,6 @@ let dirTreeNodes: ITreeNode[] = [
   }
 ];
 
-type MenuItem = {
-  text: string;
-  onClick: (e) => any;
-} & any;
-
-export type Props = {
-  items: Array<"divider" | MenuItem>;
-  commonButtonProps?: any;
-  arrowButtonProps?: any;
-  actionButtonProps?: any;
-};
-
-export type State = {
-  item: MenuItem;
-};
-
-class DropdownButton extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = { item: props.items[0] };
-  }
-  componentWillMount() {
-    const { items } = this.props;
-  }
-
-  clickItem = (e, item: MenuItem, index: number) => {
-    this.setState({ item });
-    item.onClick(e);
-  };
-
-  renderMenu = () => {
-    const { items } = this.props;
-    return (
-      <Menu className="dropdown_button_body">
-        {" "}
-        {items.map((item, index) => {
-          if (item === "divider") {
-            return <Menu.Divider key={index} />;
-          } else {
-            return (
-              <Menu.Item
-                {...item}
-                onClick={e => this.clickItem(e, item, index)}
-                key={index}
-              />
-            );
-          }
-        })}
-      </Menu>
-    );
-  };
-
-  render() {
-    const {
-      commonButtonProps,
-      arrowButtonProps,
-      actionButtonProps
-    } = this.props;
-    const { icon, text } = this.state.item;
-    return (
-      <Popover
-        minimal
-        position={Position.BOTTOM_LEFT}
-        content={this.renderMenu()}
-        hasBackdrop
-      >
-        <div className="dropdown_button">
-          <ButtonGroup fill>
-            <Button
-              {...commonButtonProps}
-              {...actionButtonProps}
-              className="dropdown_button_button"
-              onClick={e => {
-                e.stopPropagation();
-                this.state.item.onClick(e);
-              }}
-              {...{ icon, text }}
-            />
-
-            <Button
-              {...commonButtonProps}
-              {...arrowButtonProps}
-              className={`dropdown_button_arrow ${Classes.FIXED}`}
-              rightIcon="caret-down"
-            />
-          </ButtonGroup>
-        </div>
-      </Popover>
-    );
-  }
-}
 
 class App extends React.Component {
   render() {
